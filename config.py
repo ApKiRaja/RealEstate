814b92bee2859d97fb43a904768a2df9fb966209
ApKiRaja <LinkGuardianPro@gmail.com>
2025-06-13 19:23:13 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+RAZORPAY_KEY = os.getenv("RAZORPAY_KEY")
+RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET")
+DATABASE_URL = os.getenv("DATABASE_URL")
