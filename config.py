9fda9ca8f12ff68304c4149fc31c4b9367e58f83
ApKiRaja <LinkGuardianPro@gmail.com>
2025-06-13 19:30:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-RAZORPAY_KEY = os.getenv("RAZORPAY_KEY")
-RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET")
+TOKEN = os.getenv("7505185053:AAHr1ZFCub0IpFQ8TXgqrCiWmu3ReaNmnys")
+RAZORPAY_KEY = os.getenv("rzp_live_Ar6O2fpJcq8MZV")
+RAZORPAY_SECRET = os.getenv("5TmoY5R2bCggZaAelYDaOjp9")
 DATABASE_URL = os.getenv("DATABASE_URL")
