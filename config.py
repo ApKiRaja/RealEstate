834ca40bb1745bd28d2d8b74829eddb0ecf0c670
ApKiRaja <LinkGuardianPro@gmail.com>
2025-06-13 19:41:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # Loads .env file
 
-TOKEN = os.getenv("7505185053:AAHr1ZFCub0IpFQ8TXgqrCiWmu3ReaNmnys")
-RAZORPAY_KEY = os.getenv("rzp_live_Ar6O2fpJcq8MZV")
-RAZORPAY_SECRET = os.getenv("5TmoY5R2bCggZaAelYDaOjp9")
-DATABASE_URL = os.getenv("DATABASE_URL")
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+RAZORPAY_KEY = os.getenv("RAZORPAY_KEY")
+RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET")
+DATABASE_URL = os.getenv("DATABASE_URL")  # Auto-provided by Railway
